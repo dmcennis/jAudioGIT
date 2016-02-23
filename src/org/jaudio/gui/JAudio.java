92793ce36e61c094e90454b4128e4b8d5f974a69
Daniel McEnnis <daniel.mcennis@researchatscale.com>
2016-02-23 17:47:03 -0500

    Initial work on a replacement gui

---
+++ src/org/jaudio/gui/JAudio.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Daniel McEnnis on 2/23/2016
+ * <p/>
+ * Copyright Daniel McEnnis 2015
+ */
+
+package org.jaudio.gui;
+
+import javax.swing.*;
+
+/**
+ * Default Description Google Interview Project
+ */
+public class JAudio {
+
+    private JTree tree1;
+    private JTree tree2;
+    private JButton saveButton;
+    private JButton runButton;
+    private JButton addButton;
+
+    /**
+     * Default constructor for JAudio
+     */
+    public JAudio() {
+
+    }
+}
